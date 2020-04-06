1e3074010f7b97babeb76b9242a75f934232b942
Alexander <o.nadtochyi@reply.it>
2020-04-06 12:35:14 +0200

    convert to TS appReducer

--- src/redux/appReducer.ts
+++ src/redux/appReducer.ts
@@ -1,15 +1,20 @@
-import { authAPI } from '../api/api' 
 import {authThunkCreator} from './authReducer'
 
 const INITIALIZE_SUCCESS = 'SET_INITIALIZED'
 const IS_LOADING = 'IS_LOADING'
 
 const initialState = {
-    initialized: false,
-    isLoading: false,
+    initialized: false as boolean,
+    isLoading: false as boolean,
 }
 
-const appReducer = (state = initialState, action) => {
+type InitialStateType = typeof initialState
+type ActionType = {
+    type: typeof INITIALIZE_SUCCESS | typeof IS_LOADING,
+    payload: boolean
+}
+
+const appReducer = (state: InitialStateType = initialState, action:ActionType) => {
     switch (action.type) {
         case INITIALIZE_SUCCESS:
             return {
@@ -29,10 +34,10 @@ const appReducer = (state = initialState, action) => {
 }
 
 export const setInitializeSuccess = () => ({ type: INITIALIZE_SUCCESS })
-export const setLoadingStatus = (status) => ({ type: IS_LOADING, payload: status })
+export const setLoadingStatus = (status: boolean) => ({ type: IS_LOADING, payload: status })
 
 export const initializeThunkCreator = () => {
-    return (dispatch) => {
+    return (dispatch: Function) => {
         dispatch(setLoadingStatus(true))
         let getAuthPromise = dispatch(authThunkCreator())
         getAuthPromise.then(()=>{
